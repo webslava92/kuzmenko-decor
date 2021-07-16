988b30388ee7a012c0b90c52a1e5810fba2741fa
webslava92 <pustovit.slava@gmail.com>
2021-07-17 01:28:04 +0300

    add project, header, footer

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  $('.header__menu-btn').on('click', function () {
+    $('.header__menu-btn').toggleClass('header__menu-btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__link').on('click', function () {
+    $('.menu__link').removeClass('menu__link--active');
+    $(this).addClass('menu__link--active');
+    $('.header__menu-btn').toggleClass('header__menu-btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+});
