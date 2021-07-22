36ecd3077ffc88c41f46bd75ae883c12f8fe61a0
webslava92 <pustovit.slava@gmail.com>
2021-07-23 01:26:33 +0300

    add portfolio section

--- app/js/main.js
+++ app/js/main.js
@@ -15,8 +15,34 @@ $(function(){
   $('.top-slider__list').slick({
     dots: false,
     arrows: false,
-    infinite: true,
-    speed: 3000
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2000
+  });
+
+  $(".menu a, .footer a").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top - 105;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  window.onscroll = function () {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 10) {
+      $(".header__top-inner").css({ "background": "rgb(2, 51, 59, 0.9)" })
+    }
+    if (10 > scrolled) {
+      $(".header__top-inner").css({ "background": "transparent" })
+    }
+  }
+
+  $('.portfolio__slider').slick({
+    dots: false,
+    arrows: true,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2000
   });
 
 });
