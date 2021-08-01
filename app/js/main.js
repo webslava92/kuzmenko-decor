5624d95c443a4b1f51767059e3c742f606b40d29
webslava92 <pustovit.slava@gmail.com>
2021-08-01 17:01:41 +0300

    add all blocks ang updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+  $('.header__formbox-btn, .header__call-btn').on('click', function () {
+    $(this).addClass('header__formbox--active');
+    $('.header__formbox').toggleClass('header__formbox--active');
+  });
+
+  $('.header__form-btn').on('click', function () {
+    $(this).addClass('header__formbox--active');
+    $('.header__formbox').toggleClass('header__formbox--active');
+  });
+
+  $('.top-slider__btn, .portfolio__btn, price__item-btn').on('click', function () {
+    $('header__formbox').addClass('header__formbox--active');
+    $('.header__formbox').toggleClass('header__formbox--active');
+  });
+
   $('.header__menu-btn').on('click', function () {
     $('.header__menu-btn').toggleClass('header__menu-btn--active');
     $('.menu__list').toggleClass('menu__list--active');
@@ -11,6 +26,24 @@ $(function(){
     $('.header__menu-btn').toggleClass('header__menu-btn--active');
     $('.menu__list').toggleClass('menu__list--active');
   });
+
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Данные отправлены. Мы свяжемся с Вами в указанное время");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
+  new WOW().init();
 
   $('.top-slider__list').slick({
     dots: false,
@@ -45,4 +78,37 @@ $(function(){
     autoplayspeed: 2000
   });
 
+  $('.materials__list').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplayspeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 3,
+        },
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        },
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      }
+    ]
+  });
+
+  $('.reviews__list').slick({
+    dots: false,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2000
+  });
+
 });
+
